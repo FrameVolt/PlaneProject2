2594c1a473ed66ba270ae96c8502e4bf2e3af5fb
Yaom!ng <1064004998@qq.com>
2018-05-01 17:22:40 +0800

    Add ScreenXY

--- Assets/MainPlane.cs
+++ Assets/MainPlane.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainPlane : MonoBehaviour {
+    [SerializeField]
+    private float speed = 1f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int hp = 100;
+
+    private Vector3 direction;
+    private float v;
+
+    private float MaxX;
+    private float MinX;
+    private float MaxY;
+    private float MinY;
+
+    private void Start()
+    {
+        MaxX = ScreenXY.MaxX;
+        MinX = ScreenXY.MinX;
+        MaxY = ScreenXY.MaxY;
+        MinY = ScreenXY.MinY;
+    }
+
+
+    void Update () {
+        direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+        Move(direction);
+
+        ClampFrame();
+    }
+
+
+    private void Move(Vector3 direction) {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void ClampFrame() {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX, MaxX),
+                                     Mathf.Clamp(transform.position.y, MinY, MaxY),
+                                     transform.position.z);
+    }
 }
