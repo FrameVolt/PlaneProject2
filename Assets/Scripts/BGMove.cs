af00dc8156d06c01baf1e12a918f6c1761bcc6ba
Yaom!ng <1064004998@qq.com>
2018-05-04 16:54:39 +0800

    Add BG

---
+++ Assets/Scripts/BGMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class BGMove : MonoBehaviour {
+    [SerializeField]
+    private float speed = 0.3f;
+    private SpriteRenderer spriteRenderer;
+
+    private Material m_material;
+   
+
+    void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //m_material = spriteRenderer.material;
+
+        m_material = spriteRenderer.sharedMaterial;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //m_material.SetTextureOffset("_MainTex", Vector2.up * Time.time);
+        m_material.mainTextureOffset = Vector2.up * Time.time * speed;
+        
+    }
+}
--- Assets/Scripts/Managers/Director.cs
+++ Assets/Scripts/Managers/Director.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class Director : MonoBehaviour {
+public class Director : Singleton<Director> {
     [SerializeField]
     private GameObject mainPlanePrefab;
 
-    private EnemyFactoryBase enemyFactoryBase;
     
-    void Awake () {
+    [SerializeField]
+    private int score;
+    public int Score {
+        get { return score; }
+        set{
+            score = value;
 
+
+        }
+    }
+    private EnemyFactoryBase enemyFactoryBase;
+
+
+    protected override void Awake () {
+        base.Awake();
         Instantiate(mainPlanePrefab, mainPlanePrefab.transform.position, Quaternion.identity);
-
         enemyFactoryBase = new EnemyFactoryBase();
         enemyFactoryBase.CreateEnemy(EnemyType.NormalEnemy);
         enemyFactoryBase.CreateEnemy(EnemyType.Boss);
 
     }
-    private void Start()
-    {
-        
-    }
 
+    void OnGUI()
+    {
+        if (Input.anyKeyDown)
+        {
+            Event e = Event.current;
+            if (e != null && e.isKey && e.keyCode != KeyCode.None)
+            {
+                print(e.keyCode);
+            }
+        }
+    }
 }
