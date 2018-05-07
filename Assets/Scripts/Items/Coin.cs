28441aff5e116c4453956128ad1417184725d167
Yaom!ng <1064004998@qq.com>
2018-05-07 12:47:22 +0800

    add audio

---
+++ Assets/Scripts/Items/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+
+    private Collider2D coll;
+    private Renderer rend;
+    private AudioSource coinAudio;
+
+	void Awake () {
+        coll = GetComponent<Collider2D>();
+        rend = GetComponent<Renderer>();
+        coinAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            coll.enabled = false;
+            Director.Instance.Score += 10;
+            rend.enabled = false;
+            coinAudio.Play();
+            Destroy(this.gameObject, coinAudio.clip.length);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour {
+    [SerializeField]
+    private AudioMixer auidoMixer;
+
+
+	void Start () {
+        auidoMixer.SetFloat("fx",-20);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
